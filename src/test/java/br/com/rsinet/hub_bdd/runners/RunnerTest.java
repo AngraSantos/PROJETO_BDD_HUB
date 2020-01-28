582706e491bec50875cd3d3de73a86622721882c
Angra Santos <angra.santos@hotmail.com.br>
2020-01-28 16:11:36 -0300

    commit final

--- src/test/java/br/com/rsinet/hub_bdd/runners/RunnerTest.java
+++ src/test/java/br/com/rsinet/hub_bdd/runners/RunnerTest.java
@@ -13,8 +13,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/TesteFuncional", 
 glue = { "br.com.rsinet.hub_bdd.stepDefinition" },
-tags = {"@BuscaPelaLupaComFalha "},
-plugin = { "pretty", "com.cucumber.listener.ExtentCucumberFormatter:target/reports/reports.html"},
+//tags = {"@BuscaPelaLupaComFalha "},
+plugin = { "pretty", "com.cucumber.listener.ExtentCucumberFormatter:target/reports.html"},
 monochrome = true)
 
 public class RunnerTest {
